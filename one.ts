8515b6fcd8f9e3ae5ada7c960b1393065bf69ec0
hugeletters <ckuka71@gmail.com>
2024-02-25 00:24:57 +0100

    some more work

--- one.ts
+++ one.ts
@@ -1,26 +1,75 @@
-namespace Covariance {}
-namespace Bivariance {
-  // todo - this is a wrong explanation, VARIANCE IS ABOUT TYPE TRANSFORMATION, NOT TYPE ITSELF!!!!
+namespace Variance {
+  type Extends<A, B> = [A] extends [B] ? true : false;
+  type CheckTypes<A, B, K extends [boolean, boolean] = [Extends<A, B>, Extends<B, A>]> = K extends [
+    true,
+    true
+  ]
+    ? "Equivalent"
+    : K extends [true, false]
+    ? "First extends second"
+    : K extends [false, true]
+    ? "Second extends first"
+    : "Types are unrelated";
+
   //   investigate here - https://stackoverflow.com/a/66411491
-}
+  namespace Covariance {
+    type Narrow = string;
+    type Wide = string | number;
+    type Before = CheckTypes<Narrow, Wide>;
+    //    ^?
+    namespace ObjectWithProperty {
+      type WithA<T> = { a: T };
+      type After = CheckTypes<WithA<Narrow>, WithA<Wide>>;
+      //    ^?
+    }
+    namespace Array {
+      type After = CheckTypes<Array<Narrow>, Array<Wide>>;
+      //    ^?
+      type After2 = CheckTypes<ReadonlyArray<Narrow>, ReadonlyArray<Wide>>;
+      //    ^?
 
-namespace SubtypeWeirdness {
-  // When bi-extension doesn't mean equivalence
-  type BiExtended<A, B> = [A extends B ? true : false, B extends A ? true : false];
-  type A = BiExtended<{}, { a?: string }>; // wrong
-  //   ^?
-  type B = BiExtended<{ a?: string }, { b?: string }>;
-  //   ^?
-  type B2 = BiExtended<{ a?: string; c: string }, { b?: string; c: string }>; // wrong
-  //   ^?
-  type C = BiExtended<Record<never, unknown>, { b?: string }>; // wrong
-  //   ^?
-  type D = BiExtended<Record<PropertyKey, unknown>, { b?: string }>; // wrong
-  //   ^?
-  type E = BiExtended<{ b: string }, { a?: string; b: string }>; // wrong
-  //   ^?
+      type F = Array<Narrow>["unshift"];
+      type S = Array<Wide>["unshift"];
+      //   todo - this is equivalent because these are methods, not properties
+      type R = CheckTypes<F, S>;
+      //   ^?
+    }
+  }
+  namespace Contravariance {}
+  namespace Invariance {
+    // array is a good example of this - it's considered covariant by TS but actually invariant
+  }
+  namespace Bivariance {
+    namespace Method {
+      let wide = { a(x: string) {} };
+      let narrow = { a(x: string | number) {} };
+      wide = narrow;
+      narrow = wide;
+    }
+    namespace Property {
+      let wide = { a: (x: string) => {} };
+      let narrow = { a: (x: string | number) => {} };
+      wide = narrow;
+      narrow = wide;
+    }
+  }
+  namespace SubtypeWeirdness {
+    // When bi-extension doesn't mean equivalence
+    type A = CheckTypes<{}, { a?: string }>; // wrong
+    //   ^?
+    type B = CheckTypes<{ a?: string }, { b?: string }>;
+    //   ^?
+    type B2 = CheckTypes<{ a?: string; c: string }, { b?: string; c: string }>; // wrong
+    //   ^?
+    type C = CheckTypes<Record<never, unknown>, { b?: string }>; // wrong
+    //   ^?
+    type D = CheckTypes<Record<PropertyKey, unknown>, { b?: string }>; // wrong
+    //   ^?
+    type E = CheckTypes<{ b: string }, { a?: string; b: string }>; // wrong
+    //   ^?
 
-  /** Рассказать про сложности с index/never ключами - работают они не так же как конкретные ключи, тк не требуют их наличия */
+    /** Рассказать про сложности с index/never ключами - работают они не так же как конкретные ключи, тк не требуют их наличия */
+  }
 }
 
 namespace WhatIsEmptyObject {
